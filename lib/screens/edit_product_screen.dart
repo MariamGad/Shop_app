331dee3cc3dc88fcf68c81a3afac6609ee184f87
Mariamgad <mariamgad142@gmail.com>
2021-12-23 17:14:40 +0200

    handling errors

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -105,7 +105,23 @@ class _EditProductScreenState extends State<EditProductScreen> {
     } else {
       Provider.of<Products>(context, listen: false)
           .addProduct(_editedProduct)
-          .then((_) {
+          .catchError((error) {
+        return showDialog<Null>(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('An error occurred!'),
+            content: Text('Something went wrong.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Okay'),
+              ),
+            ],
+          ),
+        );
+      }).then((_) {
         setState(() {
           isLoading = false;
         });
